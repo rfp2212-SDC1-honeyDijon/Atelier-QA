2c3f3c46ef8cf03e44df8bb7187afdd3216af616
Aimee Kang <asrkang@gmail.com>
2023-02-15 11:15:21 -0800

    set up QA repo

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'airbnb',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'comma-dangle': [
+      'error',
+      'never'
+    ]
+  }
+};

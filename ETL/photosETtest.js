740a018bbd6d5bf8f7469c6394f9705dab67b0e6
Aimee Kang <asrkang@gmail.com>
2023-02-16 20:54:55 -0800

    save work for notes on photosETtest.js and logs with test data

---
+++ ETL/photosETtest.js
@@ -0,0 +1,68 @@
+const csv = require('csv-parser');
+const createCsvStringifier = require('csv-writer').createObjectCsvStringifier;
+const fs = require('fs');
+const Transform = require('stream').Transform;
+
+const csvStringifier = createCsvStringifier({
+  header: [
+    { id: 'id', title: 'id' },
+    { id: 'answer_id', title: 'answer_id' },
+    { id: 'url', title: 'url' }
+  ]
+});
+
+let readStream = fs.createReadStream('./raw_test/answer_photos_test.csv');
+let writeStream = fs.createWriteStream('./transformed_test/cleanAnswersPhotosTest.csv');
+
+class CSVCleaner extends Transform {
+  constructor(options) {
+    super(options);
+  }
+
+  _transform(chunk, encoding, next) {
+    for (let key in chunk) {
+      let trimKey = key.trim();
+      chunk[trimKey] = chunk[key];
+      if (key !== trimKey) {
+        delete chunk[key];
+      }
+    }
+
+    let onlyIdNumbers = chunk.id.replace(/\D/g, '');
+    let onlyAnswerIdNumbers = chunk.answer_id.replace(/\D/g, '');
+    chunk.id = onlyIdNumbers;
+    chunk.answer_id = onlyAnswerIdNumbers;
+
+    chunk = csvStringifier.stringifyRecords([chunk]);
+    console.log('chunk', chunk);
+    let commaCount = 0;
+    let quoteInsertIndex;
+
+    for (let i = 0; i < chunk.length; i++) {
+      if (chunk[i] === ',') {
+        commaCount++;
+        if (commaCount === 2) {
+          quoteInsertIndex = i + 1;
+        }
+      }
+    }
+
+    let text = chunk.slice(0, quoteInsertIndex) + '"' + chunk.slice(quoteInsertIndex).trim();
+    console.log('text', text);
+    let result = text.concat(`"\n`);
+    console.log('result', result);
+
+    this.push(result);
+
+    next();
+  }
+}
+
+const transformer = new CSVCleaner({ writableObjectMode: true });
+
+writeStream.write(csvStringifier.getHeaderString());
+readStream
+  .pipe(csv())
+  .pipe(transformer)
+  .pipe(writeStream)
+  .on('finish', () => { console.log('finished transforming photos'); });
--- ETL/questionsET.js
+++ ETL/questionsET.js
@@ -1,6 +1,9 @@
 const csv = require('csv-parser');
-const createCsvStringifier = require('csv-writer')
+const createCsvStringifier = require('csv-writer');
 const fs = require('fs');
+const Transform = require('stream').Transform;
 
+const csvStringifier = createCsvStringfier({
 
+});
 

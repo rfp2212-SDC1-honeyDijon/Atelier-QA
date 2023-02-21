5f152d4b64ec54e3c4b82a38c3c02d12ecd61585
Aimee Kang <asrkang@gmail.com>
2023-02-21 10:52:50 -0800

    add options to loadtest.js

--- k6/loadtest.js
+++ k6/loadtest.js
@@ -1,6 +1,12 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+// OPTIONS - more virutal users, longer duration
+export const options = {
+  vus: 10,
+  duration: '30s'
+};
+
 export default function () {
   http.get('https://test.k6.io');
   sleep(1);
